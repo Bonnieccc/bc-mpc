f0f62e45804220393d787640a458b48098131687
holly.fei@foxmail.com <holly.fei@foxmail.com>
2018-02-10 23:19:32 -0800

    monte carlo tree search

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-import numpy as np
-from data_buffer import DataBuffer, DataBuffer_general 
+a = []
 
-data_buffer = DataBuffer_general(5 ,2)
-
-for i in range(10):
-    data_buffer.add((i, i+1))
-    print(data_buffer.size)
-    print(data_buffer.sample(10))
+print(len(a))
