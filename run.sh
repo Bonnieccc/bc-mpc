5d5c10796ccb1ed21b93fc1d85baadb8bffd1c0e
ZhenghaoFei <holly.fei@foxmail.com>
2018-03-07 19:19:51 -0800

    working version

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 
 
-for learning_rate in 1e-2 1e-4 3e-4
+for ppo_lr in 1e-4 1e-3 5e-4 5e-5
 do
     mpc_horizon=7
     dyiter=200
-    name=mpc_ppo_h_${mpc_horizon}_diter_${dyiter}_mlr_${learning_rate}
+    name=mpc_ppo_h_${mpc_horizon}_diter_${dyiter}_ppo_lr_${ppo_lr}
 
-    nohup python -u train_mpc_ppo.py --dyn_iters=${dyiter} --exp_name=${name} --LEARN_REWARD=False --mpc=True --ppo=True --mpc_horizon=${mpc_horizon} --learning_rate=${learning_rate} &
+    nohup python -u train_mpc_ppo.py --dyn_iters=${dyiter} --exp_name=${name} --LEARN_REWARD=False --mpc=True --ppo=True --mpc_horizon=${mpc_horizon} --ppo_lr=${ppo_lr} &
 
 done
 
